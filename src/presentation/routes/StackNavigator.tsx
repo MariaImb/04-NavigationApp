6a08ed770b35c46823ff9a74d8e21f0e06674e08
Maria Imbert <mimbert@danaide.com.ar>
2024-07-11 12:40:55 -0300

    NavigationApp

---
+++ src/presentation/routes/StackNavigator.tsx
@@ -0,0 +1,30 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { ProductsScreen } from '../screens/products/ProductsScreen';
+import { SettingsScreen } from '../screens/settings/SettingsScreen';
+import { ProductScreen } from '../screens/products/ProductScreen';
+
+const Stack = createStackNavigator();
+export type RootStackParams = {
+  Home: undefined,
+  Product: undefined,
+  Products: undefined,
+  Settings: undefined
+}
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: true,
+      headerStyle: {
+        elevation: 0
+      }
+
+    }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
+    </Stack.Navigator>
+  );
+}
---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {useNavigation} from '@react-navigation/native';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+
+export const HomeScreen = () => {
+  const navigation = useNavigation();
+
+
+
+  return (
+    <View style={globalStyles.container}>
+      <PrimaryButton onPress={() => navigation.navigate('Products' as never)} label="Products"></PrimaryButton >
+      <PrimaryButton onPress={() => navigation.navigate('Settings' as never)} label="Settings"></PrimaryButton >
+    </View>
+  );
+};
